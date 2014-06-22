9f08ba90ccb9ebb3bd9cbbf18f26f1c42bfc06e8
David Lomelin <david.lomelin@gmail.com>
2014-06-22 00:02:08 -0700

    Unittests for various sorting functions

--- modules/t/test_Sort.py
+++ modules/t/test_Sort.py
@@ -3,17 +3,27 @@ from Algorithms.modules.Sort import Sort
 
 class TestSort(unittest.TestCase):
 	def setUp(self):
-		self.unsortedList = [5, 3, 11, 7, 11, 1, 18]
+		self.unsortedList = [5, 2, 4, 7, 1, 3, 2, 6]
 		self.pythonSorted = sorted(self.unsortedList)
 
-	def test_insertion(self):
+	def test_mergeSort(self):
 		# Function sorts list in place
-		Sort.insertion(self.unsortedList)
+		Sort.mergeSort(self.unsortedList)
 		self.assertEqual(self.unsortedList, self.pythonSorted)
 
-	def test_selection(self):
+	def test_insertionSort(self):
 		# Function sorts list in place
-		Sort.selection(self.unsortedList)
+		Sort.insertionSort(self.unsortedList)
+		self.assertEqual(self.unsortedList, self.pythonSorted)
+
+	def test_selectionSort(self):
+		# Function sorts list in place
+		Sort.selectionSort(self.unsortedList)
+		self.assertEqual(self.unsortedList, self.pythonSorted)
+
+	def test_bubbleSort(self):
+		# Function sorts list in place
+		Sort.bubbleSort(self.unsortedList)
 		self.assertEqual(self.unsortedList, self.pythonSorted)
 
 if __name__ == '__main__':
