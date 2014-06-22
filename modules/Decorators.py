c0fd568f50658c96b29563f93c16be28c81b2d50
David Lomelin <david.lomelin@gmail.com>
2014-06-22 00:04:53 -0700

    Added Memoize decorator and a parent class for decorators

---
+++ modules/Decorators.py
@@ -0,0 +1,29 @@
+import functools
+
+# Parent class for all Decorator children
+class Decorator(object):
+	def __init__(self, func):
+		self._func = func
+
+	def __call__(self, *args, **kwArgs):
+		self._func(*args, **kwArgs)
+
+	# Allows using this decorator with methods and functions
+	def __get__(self, obj, objtype):
+		return functools.partial(self.__call__, obj)
+
+# Stores computed values from decorated functions/methods and returns their values
+# if they have been previously called instead of recomputing
+class Memoize(Decorator):
+	def __init__(self, func):
+		super(Memoize, self).__init__(func)
+		self.__memo = {}
+
+	# Required to use this class as a Decorator
+	def __call__(self, *args):
+		# Call normal function and store its value
+		if not args in self.__memo:
+			self.__memo[args] = self._func(*args)
+
+		# Return stored value
+		return self.__memo[args]
