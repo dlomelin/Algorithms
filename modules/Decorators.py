b868b6ce6d3c969d14335c6b0671deb73dfd7130
David Lomelin <david.lomelin@gmail.com>
2014-06-23 17:12:33 -0700

    Modified decorators to be used with methods and functions

--- modules/Decorators.py
+++ modules/Decorators.py
@@ -5,12 +5,13 @@ class Decorator(object):
 	def __init__(self, func):
 		self._func = func
 
+	# Default behavior does nothing other than calling the decorated function
 	def __call__(self, *args, **kwArgs):
 		self._func(*args, **kwArgs)
 
 	# Allows using this decorator with methods and functions
-	def __get__(self, obj, objtype):
-		return functools.partial(self.__call__, obj)
+	def __get__(self, instance, owner):
+		return functools.partial(self.__call__, instance)
 
 # Stores computed values from decorated functions/methods and returns their values
 # if they have been previously called instead of recomputing
