c3f41e9d156a8a1991e6fbe053edba27a3bec8f2
David Lomelin <david.lomelin@gmail.com>
2014-07-03 00:19:30 -0700

    Added unittests for the new key function

--- modules/dataStructures/t/test_Heap.py
+++ modules/dataStructures/t/test_Heap.py
@@ -7,6 +7,35 @@ class TestHeap(unittest.TestCase):
 		self.sortedArrayDesc = self.sortedArrayAsc[::-1]
 		self.maxHeapedArray = [16, 14, 9, 10, 7, 8, 3, 1, 4, 2]
 		self.sortingArray = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+		self.dictList = [
+			{'name': 'Carl', 'age': 13},
+			{'name': 'Bob', 'age': 6},
+			{'name': 'Amy', 'age': 25},
+			{'name': 'Frank', 'age': 18},
+			{'name': 'David', 'age': 9},
+		]
+
+	def test_keyAge(self):
+		myHeap = Heap(self.dictList, key = lambda x: x['age'])
+		myHeap.sort()
+		self.assertEqual(list(myHeap), [
+			{'name': 'Bob', 'age': 6},
+			{'name': 'David', 'age': 9},
+			{'name': 'Carl', 'age': 13},
+			{'name': 'Frank', 'age': 18},
+			{'name': 'Amy', 'age': 25},
+		])
+
+	def test_keyName(self):
+		myHeap = Heap(self.dictList, key = lambda x: x['name'])
+		myHeap.sort()
+		self.assertEqual(list(myHeap), [
+			{'name': 'Amy', 'age': 25},
+			{'name': 'Bob', 'age': 6},
+			{'name': 'Carl', 'age': 13},
+			{'name': 'David', 'age': 9},
+			{'name': 'Frank', 'age': 18},
+		])
 
 	def test_insert(self):
 		myHeap = Heap(self.sortingArray)
