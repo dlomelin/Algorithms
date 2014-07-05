90c6cd9bf79b481848fc69ce99a8c1051bc64c21
David Lomelin <david.lomelin@gmail.com>
2014-07-05 15:55:04 -0700

    New Stack datastructure

---
+++ modules/dataStructures/t/test_Stack.py
@@ -0,0 +1,39 @@
+import unittest
+from Algorithms.modules.dataStructures.Stack import Stack
+
+class TestStack(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_push(self):
+		stackObj = Stack([1, 2, 3])
+		stackObj.push(5)
+		self.assertEqual(list(stackObj), [1, 2, 3, 5])
+
+	def test_pop(self):
+		stackObj = Stack([1, 2, 3])
+		val = stackObj.pop()
+		self.assertEqual(val, 3)
+		self.assertEqual(list(stackObj), [1, 2])
+
+	def test_pushPop(self):
+		stackObj = Stack([1, 2, 3])
+		stackObj.push(5)
+		stackObj.push(12)
+		stackObj.push(7)
+		val = stackObj.pop()
+		self.assertEqual(val, 7)
+		val = stackObj.pop()
+		self.assertEqual(val, 12)
+		self.assertEqual(list(stackObj), [1, 2, 3, 5])
+
+	def test_emptyTrue(self):
+		stackObj = Stack()
+		self.assertTrue(stackObj.empty())
+
+	def test_emptyFalse(self):
+		stackObj = Stack([2, 5, 13])
+		self.assertFalse(stackObj.empty())
+
+if __name__ == '__main__':
+	unittest.main()
