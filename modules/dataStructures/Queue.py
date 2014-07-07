5d013c9549a35b1b308470eab5063d6a9a462f8f
David Lomelin <david.lomelin@gmail.com>
2014-07-07 00:02:11 -0700

    Created new queue data structure

---
+++ modules/dataStructures/Queue.py
@@ -0,0 +1,74 @@
+class Queue(object):
+	def __init__(self, array = []):
+		self.__array = array
+		self.__setIndices()
+
+
+	########################
+	# Operator Overloading #
+	########################
+
+	def __eq__(self, other):
+		return list(self) == list(other)
+
+	def __str__(self):
+		# Calls __iter__ and converts to a string
+		return str(list(self))
+
+	def __len__(self):
+		return self.__tail() - self.__head()
+
+	def __iter__(self):
+		for i in xrange(self.__head(), self.__tail()):
+			yield self[i]
+
+	def __getitem__(self, index):
+		return self.__array[index]
+
+	##################
+	# Public Methods #
+	##################
+
+	def enqueue(self, value):
+		try:
+			self[self.__tail()] = value
+		except:
+			self.__array.append(value)
+		self.__increaseTail()
+
+	def dequeue(self):
+		if self.empty():
+			# TODO change to return None instead?
+			raise Exception('No items left to dequeue in queue')
+		else:
+			self.__increaseHead()
+			return self[self.__head() - 1]
+
+	# Returns True or False if the Queue is empty
+	def empty(self):
+		if self.__head() == self.__tail():
+			return True
+		else:
+			return False
+
+	###################
+	# Private Methods #
+	###################
+
+	def __setIndices(self):
+		self.__setHead(0)
+		self.__setTail(len(self.__array))
+
+	def __head(self):
+		return self.__headIndex
+	def __setHead(self, value):
+		self.__headIndex = value
+	def __increaseHead(self):
+		self.__headIndex += 1
+
+	def __tail(self):
+		return self.__tailIndex
+	def __setTail(self, value):
+		self.__tailIndex = value
+	def __increaseTail(self):
+		self.__tailIndex += 1
---
+++ modules/dataStructures/t/test_Queue.py
@@ -0,0 +1,36 @@
+import unittest
+from Algorithms.modules.dataStructures.Queue import Queue
+
+class TestQueue(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_endequeue(self):
+		qObj = Queue([1, 2, 3])
+		qObj.enqueue(5)
+		qObj.enqueue(12)
+		qObj.dequeue()
+		qObj.dequeue()
+		self.assertTrue(list(qObj), [3, 5, 12])
+
+	def test_enqueue(self):
+		qObj = Queue([1, 2, 3])
+		qObj.enqueue(5)
+		self.assertEqual(list(qObj), [1, 2, 3, 5])
+
+	def test_dequeue(self):
+		qObj = Queue([1, 2, 3])
+		val = qObj.dequeue()
+		self.assertEqual(val, 1)
+		self.assertEqual(list(qObj), [2, 3])
+
+	def test_emptyTrue(self):
+		qObj = Queue()
+		self.assertTrue(qObj.empty())
+
+	def test_emptyFalse(self):
+		qObj = Queue([1])
+		self.assertFalse(qObj.empty())
+
+if __name__ == '__main__':
+	unittest.main()
