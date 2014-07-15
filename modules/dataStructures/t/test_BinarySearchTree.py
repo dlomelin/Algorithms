347c49e6caf25102eac362a1206816eb2a04037c
David Lomelin <david.lomelin@gmail.com>
2014-07-15 08:40:51 -0700

    Added testing for NodeBST successor methods

--- modules/dataStructures/t/test_BinarySearchTree.py
+++ modules/dataStructures/t/test_BinarySearchTree.py
@@ -1,6 +1,6 @@
 import unittest
 from Algorithms.modules.dataStructures.BinarySearchTree import BinarySearchTree
-from Algorithms.modules.dataStructures.Node import Node
+from Algorithms.modules.dataStructures.NodeBST import NodeBST
 
 class TestBinarySearchTree(unittest.TestCase):
 	def setUp(self):
@@ -45,13 +45,34 @@ class TestBinarySearchTree(unittest.TestCase):
 		node = self.bstObj.search(searchValue)
 		self.assertTrue(node is None)
 
+	def test_successor1(self):
+		# Load standard data
+		self.__loadData()
+
+		searchValue = 15
+		node = self.bstObj.search(searchValue)
+
+		successorNode = node.successor()
+		self.assertEqual(successorNode.value(), 17)
+
+	def test_successor2(self):
+		# Load standard data
+		self.__loadData()
+
+		searchValue = 13
+		node = self.bstObj.search(searchValue)
+
+		successorNode = node.successor()
+		self.assertEqual(successorNode.value(), 15)
+
+
 	###################
 	# Private Methods #
 	###################
 
 	def __loadData(self):
 		for i in [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]:
-			self.bstObj.insert(Node(i))
+			self.bstObj.insert(NodeBST(i))
 
 if __name__ == '__main__':
 	unittest.main()
