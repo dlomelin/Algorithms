d40663959076e269587758645949bbc45b4414ba
David Lomelin <david.lomelin@gmail.com>
2014-07-20 09:18:34 -0700

    Added code for creating a priority tree based on frequency data

--- modules/Huffman.py
+++ modules/Huffman.py
@@ -1,4 +1,37 @@
 from Algorithms.modules.PriorityQueue import PriorityQueueMin
+from Algorithms.modules.dataStructures.Node import Node
 
-def huffman():
-	print 'huffman() called'
+def huffman(dataList):
+
+	listLength = len(dataList)
+
+	pqmObj = PriorityQueueMin(array = dataList, key = lambda x: x.value()['freq'])
+
+	print 'Starting tree:'
+	for node in pqmObj:
+		print node
+	print 'Done'
+	print '=========='
+
+	for i in range(listLength-1):
+		leftMin = pqmObj.extractTopQueue()
+		rightMin = pqmObj.extractTopQueue()
+
+		print 'Topleft: %s' % (leftMin)
+		print 'Topright: %s' % (rightMin)
+
+		# Add the left and right node frequencies as the new frequency
+		# and create a new node
+		newFreq = leftMin.value()['freq'] + rightMin.value()['freq']
+		node = Node({'freq': newFreq}, lNode = leftMin, rNode = rightMin)
+
+		print 'Inserting node: ',
+		print node
+
+		pqmObj.insert(node)
+
+		print 'Updated tree:'
+		for node in pqmObj:
+			print node
+		print 'Done'
+		print '=========='
--- modules/t/test_Huffman.py
+++ modules/t/test_Huffman.py
@@ -1,12 +1,21 @@
 import unittest
 import Algorithms.modules.Huffman as Huffman
+from Algorithms.modules.dataStructures.Node import Node
 
 class TestHuffman(unittest.TestCase):
 	def setUp(self):
 		pass
 
 	def test_huffman(self):
-		Huffman.huffman()
+		dataList = [
+			Node({'char': 'a', 'freq': 45}),
+			Node({'char': 'b', 'freq': 13}),
+			Node({'char': 'c', 'freq': 12}),
+			Node({'char': 'd', 'freq': 16}),
+			Node({'char': 'e', 'freq': 9}),
+			Node({'char': 'f', 'freq': 5}),
+		]
+		Huffman.huffman(dataList)
 
 if __name__ == '__main__':
 	unittest.main()
