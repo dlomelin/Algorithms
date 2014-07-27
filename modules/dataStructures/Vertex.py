1b9374a2513e1bdb21e906449cac5e7877db5ecc
David Lomelin <david.lomelin@gmail.com>
2014-07-27 10:52:43 -0700

    Added tracking for adjacent vertex

--- modules/dataStructures/Vertex.py
+++ modules/dataStructures/Vertex.py
@@ -1,23 +1,33 @@
 from Algorithms.modules.dataStructures.mixins.MixinEquality import MixinEquality
 
 class Vertex(MixinEquality):
-	def __init__(self):
+	def __init__(self, key, adjacencyList):
+		self.__key = key
+		self.__adjacencyList = adjacencyList
+
 		self.setStatus()
-		self.setParent()
+		self.setPredecessor()
+		self.setDistance()
 
 	########################
 	# Operator Overloading #
 	########################
 
-	#def __repr__(self):
-	#	return '%s(%s)' % (self.__class__.__name__, self.value())
-
 	def __str__(self):
-		return ''
+		return 'Vertex: %s  Neighbors: %s  Distance: %s  Predecessor: %s' % (
+			self.__key,
+			self.__adjacencyList,
+			self.getDistance(),
+			self.getPredecessor(),
+		)
 
 	##################
 	# Public Methods #
 	##################
+
+	def adjacencies(self):
+		for vertexKey in self.__adjacencyList:
+			yield vertexKey
 
 	def getStatus(self):
 		return self.__status
@@ -25,11 +35,17 @@ class Vertex(MixinEquality):
 	def setStatus(self, status = None):
 		self.__status = status
 
-	def getParent(self):
-		return self.__parent
+	def getPredecessor(self):
+		return self.__predecessor
 
-	def setParent(self, vertex = None):
-		self.__parent = vertex
+	def setPredecessor(self, vertex = None):
+		self.__predecessor = vertex
+
+	def getDistance(self):
+		return self.__distance
+
+	def setDistance(self, distance = 0):
+		self.__distance = distance
 
 	###################
 	# Private Methods #
