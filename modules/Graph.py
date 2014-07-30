f746761a0449c9bfaab0140abb82cc0ce3bfcfe7
David Lomelin <david.lomelin@gmail.com>
2014-07-30 07:45:33 -0700

    Added method to print out path to node and return Vertex objects

--- modules/Graph.py
+++ modules/Graph.py
@@ -5,19 +5,31 @@ class Graph(object):
 	def __init__(self):
 		self.__vertices = {}
 
+	########################
+	# Operator Overloading #
+	########################
+
+	def __iter__(self):
+		for key in self.__vertices.keys():
+			yield self.getVertex(key)
+
+	##################
+	# Public Methods #
+	##################
+
 	# Breadth first search
 	def bfs(self, rootVertexKey):
 
 		# Reset all vertices
-		for key in self.__vertices.keys():
-			self.__vertices[key].setStatus('unvisited')
-			self.__vertices[key].setDistance(float('inf'))
-			self.__vertices[key].setPredecessor(None)
+		for vertex in self:
+			vertex.setStatus('unvisited')
+			vertex.setDistance(float('inf'))
+			vertex.setPredecessor(None)
 
 		# Mark user specified vertex as the first queued vertex
-		self.__vertices[rootVertexKey].setStatus('queued')
-		self.__vertices[rootVertexKey].setDistance(0)
-		self.__vertices[rootVertexKey].setPredecessor(None)
+		self.getVertex(rootVertexKey).setStatus('queued')
+		self.getVertex(rootVertexKey).setDistance(0)
+		self.getVertex(rootVertexKey).setPredecessor(None)
 
 		queueObj = Queue()
 		queueObj.enqueue(rootVertexKey)
@@ -25,13 +37,11 @@ class Graph(object):
 		while not queueObj.empty():
 			# Remove 1 vertex from the queue
 			vertexKey = queueObj.dequeue()
-			currentVertex = self.__vertices[vertexKey]
-
-			print currentVertex
+			currentVertex = self.getVertex(vertexKey)
 
 			# Iterate through each neighboring vertex
 			for adjVertexKey in currentVertex.adjacencies():
-				adjVertex = self.__vertices[adjVertexKey]
+				adjVertex = self.getVertex(adjVertexKey)
 
 				# If that node has never been visited, modify it and add it to the queue
 				if adjVertex.getStatus() == 'unvisited':
@@ -44,6 +54,27 @@ class Graph(object):
 			# Mark the current vertex as visited
 			currentVertex.setStatus('visited')
 
+	# Returns a list of vertexKeys that show the path from the bfs node to the user specified key
+	def getPath(self, vertexKey):
+		vertexKeys = []
 
+		# Only perform operation if vertexKey has been found from the starting node
+		if not self.getVertex(vertexKey).getDistance() == float('inf'):
+
+			while not vertexKey is None:
+				vertexKeys.insert(0, vertexKey)
+				vertex = self.getVertex(vertexKey)
+				vertexKey = vertex.getPredecessor()
+
+		return vertexKeys
+
+	# Returns a Vertex() object stored using vertexKey identifier
+	def getVertex(self, vertexKey):
+		try:
+			return self.__vertices[vertexKey]
+		except:
+			raise Exception('Invalid vertex key specified: %s' % (vertexKey))
+
+	# Adds a Vertex() object and stores it using vertexKey as its identifier
 	def addVertex(self, vertexKey, vertex):
 		self.__vertices[vertexKey] = vertex
